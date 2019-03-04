13fd585b150348c193e1fc2a45dea9d40b484741
jmo <40837639+jmo927@users.noreply.github.com>
2019-03-04 17:37:35 -0600

    updating projects and resume

---
+++ public/assets/javascript/prev-projects.js
@@ -0,0 +1,15 @@
+const projects = [{
+    name: "Order Up",
+    image: "./assets/images/burger.jpg",
+    url: "https://shielded-sea-98495.herokuapp.com/",
+    git: "https://github.com/jmo927/order-up",
+    text: "Full-stack burger devourer.  Enter a burger, devour the burger, watch it pop up on the other side of the screen.  Uses Express, MVC/ORM, Node.js, HTML/Bootstrap."
+  }, {
+    name: "LIRI",
+    image: "./assets/images/liri.png",
+    url: "https://github.com/jmo927/LIRI-Node-Ap/",
+    git: "https://github.com/jmo927/LIRI-Node-App",
+    text: "CLI app built in Node.js that makes a variety of API calls for fun and profit."
+  }, 
+
+]
--- public/assets/javascript/projects.js
+++ public/assets/javascript/projects.js
@@ -24,12 +24,6 @@ let projectList = [
     git: "https://github.com/jmo927/CupOfSugar",
     text: "Full-stack project bringing people together through trading goods and services.  Uses Node.js, Express, MySQL, Passport.js, HTML, CSS, and jQuery/JavaScript"
   }, {
-    name: "Order Up",
-    image: "./assets/images/burger.jpg",
-    url: "https://shielded-sea-98495.herokuapp.com/",
-    git: "https://github.com/jmo927/order-up",
-    text: "Full-stack burger devourer.  Enter a burger, devour the burger, watch it pop up on the other side of the screen.  Uses Express, MVC/ORM, Node.js, HTML/Bootstrap."
-  }, {
     name: "Gryffindor Friend Finder",
     image: "./assets/images/gryff.png",
     url: "https://mysterious-lowlands-73139.herokuapp.com/",
@@ -41,12 +35,6 @@ let projectList = [
     url: "https://github.com/jmo927/bamazon",
     git: "https://github.com/jmo927/bamazon",
     text: "CLI inventory management tool built in Node.js and MySQL."
-  }, {
-    name: "LIRI",
-    image: "./assets/images/liri.png",
-    url: "https://github.com/jmo927/LIRI-Node-Ap/",
-    git: "https://github.com/jmo927/LIRI-Node-App",
-    text: "CLI app built in Node.js that makes a variety of API calls for fun and profit."
   }, {
     name: "Movie-Night",
     image: "./assets/images/movie.jpg",
